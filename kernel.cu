749383da8be0472ad73f1b12340a0e9b1915650c
Cheer Xiao <xiaqqaix@gmail.com>
2014-04-16 20:30:06 +0800

    Implement k-NN filter

--- kernel.cu
+++ kernel.cu
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -8,7 +9,13 @@ extern "C" {
 enum {
 	HALF_WINDOW = 2,
 	BACKLOG_SIZE = 5,
+	KNN_NELEMS = 5,
 };
+
+typedef struct {
+	uint32_t v;
+	double d;
+} knnElem;
 
 typedef struct {
 	uint32_t *backlogs[BACKLOG_SIZE+1];
@@ -23,9 +30,9 @@ __device__ inline static double distance(int32_t u, int32_t v) {
 
 #define in(i, j) in[i*w + j]
 #define out(i, j) out[i*w + j]
-#define FOR_WINDOW(i2, j2, i, j, h, w) \
-	for (i2 = max(0, i - HALF_WINDOW); min(h, i2 < i + HALF_WINDOW); i2++) \
-		for (j2 = max(0, j - HALF_WINDOW); min(w, j2 < j + HALF_WINDOW); j2++)
+#define FOR_WINDOW(i2, j2, i, j, h, w, wd) \
+	for (i2 = max(0, i - wd); min(h, i2 < i + wd); i2++) \
+		for (j2 = max(0, j - wd); min(w, j2 < j + wd); j2++)
 
 __global__ void denoiseKernelSpatial(
 		const uint32_t *in, uint32_t *out, int h,
@@ -40,9 +47,9 @@ __global__ void denoiseKernelSpatial(
 		int i = k / w, j = k % w;
 		double dmin = INFINITY;
 		int i2, j2, i3, j3;
-		FOR_WINDOW(i2, j2, i, j, h, w) {
+		FOR_WINDOW(i2, j2, i, j, h, w, HALF_WINDOW) {
 			double d = 0;
-			FOR_WINDOW(i3, j3, i, j, h, w) {
+			FOR_WINDOW(i3, j3, i, j, h, w, HALF_WINDOW) {
 				d += distance(in(i2, j2), in(i3, j3));
 			}
 			if (dmin > d) {
@@ -172,9 +179,71 @@ __global__ void denoiseKernelZlokolica(
 	}
 }
 
+__device__ bool said = false;
+
 __global__ void denoiseKernelKNN(
-		KernelContext *ctx, uint32_t *out,
+		KernelContext *ctx, int nbacklogs, uint32_t *out,
 		int h, int w, int perThread) {
+	int k = blockDim.x * blockIdx.x + threadIdx.x;
+	int lower = k * perThread;
+	int upper = (k + 1) * perThread;
+	if (k == 256 * 32 - 1) {
+		upper = h * w;
+	}
+	uint32_t **backlogs = ctx->backlogs;
+	// Use at most 3 backlogs
+	if (nbacklogs > 3) {
+		nbacklogs = 3;
+	}
+	for (k = lower; k < upper; k++) {
+		int i = k / w, j = k % w;
+		uint32_t center;
+		if (nbacklogs == 1) {
+			center = backlogs[0][k];
+		} else {
+			center = backlogs[1][k];
+		}
+		knnElem elems[KNN_NELEMS+1];
+		int nelems = 0;
+		int i2, j2, t, e;
+		FOR_WINDOW(i2, j2, i, j, h, w, 1) {
+			for (t = 0; t < nbacklogs; t++) {
+				uint32_t v = backlogs[t][i2*w+j2];
+				double d = distance(v, center);
+				// Insert v into elems
+				for (e = nelems-1; e >= 0 && elems[e].d > d; e--) {
+					/*
+					if (e < KNN_NELEMS-1) {
+						elems[e+1] = elems[e];
+					}
+					*/
+					elems[e+1] = elems[e];
+				}
+				if (1) {
+				//if (e < KNN_NELEMS-1) {
+					elems[e+1].v = v;
+					elems[e+1].d = d;
+				}
+				if (nelems < KNN_NELEMS) {
+					nelems++;
+				}
+			}
+		}
+		uint32_t r = 0, g = 0, b = 0;
+		for (e = 0; e < nelems; e++) {
+			uint32_t v = elems[e].v;
+			r += v & 0xff;
+			g += (v >> 8) & 0xff;
+			b += (v >> 16) & 0xff;
+		}
+		r /= e;
+		g /= e;
+		b /= e;
+		r &= 0xff;
+		g &= 0xff;
+		b &= 0xff;
+		out[k] = r | (g << 8) | (b << 16);
+	}
 }
 
 void denoise(void *p, int m, const uint32_t *in, uint32_t *out, int h, int w) {
@@ -198,6 +267,8 @@ void denoise(void *p, int m, const uint32_t *in, uint32_t *out, int h, int w) {
 		break;
 	case TEMPORAL_AVG:
 	case ADAPTIVE_TEMPORAL_AVG:
+	case KNN:
+	case ZLOKOLICA:
 		// Cyclically right shift backlog
 		for (int i = BACKLOG_SIZE; i > 0; i--) {
 			ctx->backlogs[i] = ctx->backlogs[i-1];
@@ -211,6 +282,9 @@ void denoise(void *p, int m, const uint32_t *in, uint32_t *out, int h, int w) {
 		KernelContext *dctx;
 		cudaMalloc(&dctx, sizeof(KernelContext));
 		cudaMemcpy(dctx, ctx, sizeof(KernelContext), cudaMemcpyHostToDevice);
+		int i;
+		for (i = 0; i < BACKLOG_SIZE && ctx->backlogs[i]; i++)
+			;
 		switch (m) {
 		case TEMPORAL_AVG:
 			denoiseKernelTemporalAvg<<<blocksPerGrid,
@@ -218,16 +292,13 @@ void denoise(void *p, int m, const uint32_t *in, uint32_t *out, int h, int w) {
 			break;
 		case KNN:
 			denoiseKernelKNN<<<blocksPerGrid,
-				threadsPerBlock>>>(dctx, dout, h, w, perThread);
+				threadsPerBlock>>>(dctx, i, dout, h, w, perThread);
 			break;
 		case ZLOKOLICA:
 			denoiseKernelZlokolica<<<blocksPerGrid,
 				threadsPerBlock>>>(dctx, dout, h, w, perThread);
 			break;
 		case ADAPTIVE_TEMPORAL_AVG:
-			int i;
-			for (i = 0; i < BACKLOG_SIZE && ctx->backlogs[i]; i++)
-				;
 			denoiseKernelAdaptiveTemporalAvg<<<blocksPerGrid,
 				threadsPerBlock>>>(dctx, i, dout, h, w, perThread);
 			break;
