344cf1fe13a0fff95f071d0f210e672ec2816b60
Cheer Xiao <xiaqqaix@gmail.com>
2014-05-10 11:27:09 +0800

    Whatever

--- kernel.cu
+++ kernel.cu
@@ -9,7 +9,9 @@ extern "C" {
 enum {
 	HALF_WINDOW = 2,
 	BACKLOG_SIZE = 5,
-	KNN_NELEMS = 5,
+	NDIFFS = 2,
+	KNN_NELEMS = 10,
+	KNN_FRAMES = 5,
 };
 
 typedef struct {
@@ -19,6 +21,7 @@ typedef struct {
 
 typedef struct {
 	uint32_t *backlogs[BACKLOG_SIZE+1];
+	uint32_t *diffs[NDIFFS+1];
 } KernelContext;
 
 __device__ inline static double distance(int32_t u, int32_t v) {
@@ -28,8 +31,80 @@ __device__ inline static double distance(int32_t u, int32_t v) {
 	return sqrt(double(r * r + g * g + b * b));
 }
 
-#define in(i, j) in[i*w + j]
-#define out(i, j) out[i*w + j]
+__device__ uint32_t diff(uint32_t u, uint32_t v) {
+	double d = distance(u, v);
+	return d * 0x10000;
+}
+
+__device__ uint32_t lum(uint32_t u) {
+	int r = u & 0xff,
+		g = (u >> 8) & 0xff,
+		b = (u >> 16) & 0xff;
+	int l = 0.299 * r + 0.587 * g + 0.114 * b;
+	return l > 0xff ? 0xff : l;
+}
+
+__device__ uint32_t diffPixel(uint32_t u, uint32_t v) {
+	int ur = u & 0xff, ug = (u >> 8) & 0xff, ub = (u >> 16) & 0xff;
+	int vr = v & 0xff, vg = (v >> 8) & 0xff, vb = (v >> 16) & 0xff;
+	/*
+	int r = (ur - vr + 0xff) / 2;
+	int g = (ug - vg + 0xff) / 2;
+	int b = (ub - vb + 0xff) / 2;
+	*/
+	int r = abs(ur - vr);
+	int g = abs(ug - vg);
+	int b = abs(ub - vb);
+	return r | (g << 8) | (b << 16);
+	/*
+	int ur = u & 0xff, ug = (u >> 8) & 0xff, ub = (u >> 16) & 0xff;
+	int vr = v & 0xff, vg = (v >> 8) & 0xff, vb = (v >> 16) & 0xff;
+	int prod = ur * vr + ug * vg + ub * vb;
+	double um = sqrt(double(ur*ur + ug*ug + ub*ub));
+	double vm = sqrt(double(vr*vr + vg*vg + vb*vb));
+	double arg = acos(prod / (um * vm));
+	int p = arg / (3.1415926/2) * 0xff;
+	if (p > 0xff) {
+		p = 0xff;
+	}
+	*/
+	/*
+	int p = lum(u) - lum(v);
+	if (p < 0) {
+		p = -p;
+	}
+	*/
+	/*
+	double d = distance(u, v);
+	uint32_t p = d / 1.732;
+	if (p > 0xff) {
+		p = 0xff;
+	}
+	*/
+	// return p | (p << 8) | (p << 16);
+}
+
+__global__ static void updateDiff(
+		KernelContext *ctx, int h, int w, int perThread) {
+	int k = blockDim.x * blockIdx.x + threadIdx.x;
+	int lower = k * perThread;
+	int upper = (k + 1) * perThread;
+	if (k == 256 * 32 - 1) {
+		upper = h * w;
+	}
+	uint32_t *d0 = ctx->diffs[0];
+	uint32_t *b0 = ctx->backlogs[0];
+	uint32_t *b1 = ctx->backlogs[1];
+	if (b0 == 0 || b1 == 0) {
+		return;
+	}
+	for (k = lower; k < upper; k++) {
+		d0[k] = diff(b0[k], b1[k]);
+	}
+}
+
+#define in(i, j) in[(i)*w + (j)]
+#define out(i, j) out[(i)*w + (j)]
 #define FOR_WINDOW(i2, j2, i, j, h, w, wd) \
 	for (i2 = max(0, i - wd); min(h, i2 < i + wd); i2++) \
 		for (j2 = max(0, j - wd); min(w, j2 < j + wd); j2++)
@@ -59,8 +134,46 @@ __global__ void denoiseKernelSpatial(
 		}
 	}
 }
-#undef in
-#undef out
+
+__global__ void denoiseKernelSobel(
+		const uint32_t *in, int frame, double *sout, uint32_t *out, int h,
+		int w, int perThread) {
+	int k = blockDim.x * blockIdx.x + threadIdx.x;
+	int lower = k * perThread;
+	int upper = (k + 1) * perThread;
+	if (k == 256 * 32 - 1) {
+		upper = h * w;
+	}
+	for (k = lower; k < upper; k++) {
+		int x = k / w, y = k % w;
+		if (x == 0 || x == h-1 || y == 0 || y == w-1) {
+			out[k] = 0;
+			if (sout != 0) {
+				sout[k] = 0;
+			}
+			continue;
+		}
+		int gx = lum(in(x-1, y+1)) + 2 * lum(in(x, y+1)) + lum(in(x+1, y+1))
+			   - lum(in(x-1, y-1)) - 2 * lum(in(x, y-1)) - lum(in(x+1, y-1));
+		int gy = lum(in(x-1, y-1)) + 2 * lum(in(x-1, y)) + lum(in(x-1, y+1))
+			   - lum(in(x+1, y-1)) - 2 * lum(in(x+1, y)) - lum(in(x+1, y+1));
+		/*
+		int g = sqrt((gx * gx + gy * gy) / 50.0);
+		*/
+		int g = ((gx * gx + gy * gy) > 2000) * 0xff;
+		/*
+		if (g > 0x5) {
+			g = 0xff;
+		} else {
+			g = 0;
+		}
+		*/
+		out[k] = g | (g << 8) | (g << 16);
+		if (sout != 0) {
+			sout[k] = sqrt(double(gx * gx + gy * gy));
+		}
+	}
+}
 
 __global__ void denoiseKernelTemporalAvg(
 		KernelContext *ctx, uint32_t *out,
@@ -160,13 +273,54 @@ __global__ void denoiseKernelAdaptiveTemporalAvg(
 		g &= 0xff;
 		b &= 0xff;
 
-		// r = g = b = 0xff * (i - j) / nbacklogs;
+		//r = g = b = 0xff * (i - j) / nbacklogs;
 		out[k] = r | (g << 8) | (b << 16);
 	}
 }
 
+enum {
+	Z_THRM = 20 * 0x10000,
+	Z_THRD = 10,
+};
+
+__global__ void denoiseKernelMotion(
+		KernelContext *ctx, int nbacklogs, uint32_t *out,
+		int h, int w, int perThread) {
+	int k = blockDim.x * blockIdx.x + threadIdx.x;
+	int lower = k * perThread;
+	int upper = (k + 1) * perThread;
+	if (k == 256 * 32 - 1) {
+		upper = h * w;
+	}
+	uint32_t **diffs = ctx->diffs;
+	for (k = lower; k < upper; k++) {
+		int i = k / w, j = k % w;
+		int i2, j2;
+		int t;
+		// Motion detection
+		for (t = 0; t < NDIFFS && diffs[t]; t++) {
+			if (diffs[t][k] < Z_THRM) {
+				continue;
+			}
+			int excess = 0;
+			FOR_WINDOW(i2, j2, i, j, h, w, 1) {
+				int k2 = i2 * w + j2;
+				if (k2 != k && diffs[t][k2] >= Z_THRM) {
+					excess++;
+					if (excess == 2) {
+						goto after_md;
+					}
+				}
+			}
+		}
+after_md:
+		int v = 0xff * (NDIFFS - t) / NDIFFS;
+		out[k] = v | (v << 8) | (v << 16);
+	}
+}
+
 __global__ void denoiseKernelZlokolica(
-		KernelContext *ctx, uint32_t *out,
+		KernelContext *ctx, int nbacklogs, uint32_t *out,
 		int h, int w, int perThread) {
 	int k = blockDim.x * blockIdx.x + threadIdx.x;
 	int lower = k * perThread;
@@ -175,7 +329,109 @@ __global__ void denoiseKernelZlokolica(
 		upper = h * w;
 	}
 	uint32_t **backlogs = ctx->backlogs;
+	uint32_t *in = backlogs[0];
+	uint32_t **diffs = ctx->diffs;
 	for (k = lower; k < upper; k++) {
+		if (nbacklogs == 1) {
+			out[k] = backlogs[0][k];
+			continue;
+		}
+
+		int i = k / w, j = k % w;
+		int i2, j2;
+		int nf;
+		// Motion detection
+		for (nf = 0; nf < NDIFFS && diffs[nf]; nf++) {
+			if (diffs[nf][k] < Z_THRM) {
+				continue;
+			}
+			int excess = 0;
+			FOR_WINDOW(i2, j2, i, j, h, w, 1) {
+				int k2 = i2 * w + j2;
+				if (k2 != k && diffs[nf][k2] >= Z_THRM) {
+					excess++;
+					if (excess == 2) {
+						goto after_md;
+					}
+				}
+			}
+		}
+after_md:
+		if (nf < nbacklogs-1) {
+			nf++;
+		}
+
+		// Edge detection
+		bool edge = false;
+		if (i > 0 && i < h-1 && j > 0 && j < w-1) {
+			int gi = lum(in(i-1, j+1)) + 2 * lum(in(i, j+1)) + lum(in(i+1, j+1))
+				   - lum(in(i-1, j-1)) - 2 * lum(in(i, j-1)) - lum(in(i+1, j-1));
+			int gj = lum(in(i-1, j-1)) + 2 * lum(in(i-1, j)) + lum(in(i-1, j+1))
+				   - lum(in(i+1, j-1)) - 2 * lum(in(i+1, j)) - lum(in(i+1, j+1));
+			edge = (gi * gi + gj * gj) > 1250;
+		}
+
+		uint32_t center = backlogs[0][k];
+		knnElem elems[KNN_NELEMS+1];
+		int nelems = 0;
+		int t, e;
+		FOR_WINDOW(i2, j2, i, j, h, w, 1) {
+			for (t = 0; t < nf; t++) {
+				uint32_t v = backlogs[t][i2*w+j2];
+				double d = distance(v, center);
+				if (edge && d > Z_THRD) {
+					continue;
+				}
+				// Insert v into elems
+				for (e = nelems-1; e >= 0 && elems[e].d > d; e--) {
+					if (e < KNN_NELEMS-1) {
+						elems[e+1] = elems[e];
+					}
+				}
+				if (e < KNN_NELEMS-1) {
+					elems[e+1].v = v;
+					elems[e+1].d = d;
+				}
+				if (nelems < KNN_NELEMS) {
+					nelems++;
+				}
+			}
+		}
+		uint32_t r = 0, g = 0, b = 0;
+		for (e = 0; e < nelems; e++) {
+			uint32_t v = elems[e].v;
+			r += v & 0xff;
+			g += (v >> 8) & 0xff;
+			b += (v >> 16) & 0xff;
+		}
+		r /= e;
+		g /= e;
+		b /= e;
+		r &= 0xff;
+		g &= 0xff;
+		b &= 0xff;
+		out[k] = r | (g << 8) | (b << 16);
+	}
+}
+
+__global__ void denoiseKernelDiff(
+		KernelContext *ctx, int nbacklogs, uint32_t *out,
+		int h, int w, int perThread) {
+	int k = blockDim.x * blockIdx.x + threadIdx.x;
+	int lower = k * perThread;
+	int upper = (k + 1) * perThread;
+	if (k == 256 * 32 - 1) {
+		upper = h * w;
+	}
+	if (nbacklogs > 1) {
+		uint32_t **backlogs = ctx->backlogs;
+		for (k = lower; k < upper; k++) {
+			out[k] = diffPixel(backlogs[0][k], backlogs[1][k]);
+		}
+	} else {
+		for (k = lower; k < upper; k++) {
+			out[k] = 0;
+		}
 	}
 }
 
@@ -189,9 +445,9 @@ __global__ void denoiseKernelKNN(
 		upper = h * w;
 	}
 	uint32_t **backlogs = ctx->backlogs;
-	// Use at most 3 backlogs
-	if (nbacklogs > 3) {
-		nbacklogs = 3;
+	// Use at most KNN_FRAMES frames
+	if (nbacklogs > KNN_FRAMES) {
+		nbacklogs = KNN_FRAMES;
 	}
 	for (k = lower; k < upper; k++) {
 		int i = k / w, j = k % w;
@@ -210,15 +466,11 @@ __global__ void denoiseKernelKNN(
 				double d = distance(v, center);
 				// Insert v into elems
 				for (e = nelems-1; e >= 0 && elems[e].d > d; e--) {
-					/*
 					if (e < KNN_NELEMS-1) {
 						elems[e+1] = elems[e];
 					}
-					*/
-					elems[e+1] = elems[e];
 				}
-				if (1) {
-				//if (e < KNN_NELEMS-1) {
+				if (e < KNN_NELEMS-1) {
 					elems[e+1].v = v;
 					elems[e+1].d = d;
 				}
@@ -245,6 +497,7 @@ __global__ void denoiseKernelKNN(
 }
 
 void denoise(void *p, int m, const uint32_t *in, uint32_t *out, int h, int w) {
+	static int frame = 0;
 	KernelContext *ctx = (KernelContext*) p;
 	int threadsPerBlock = 256;
 	int blocksPerGrid = 32;
@@ -256,17 +509,47 @@ void denoise(void *p, int m, const uint32_t *in, uint32_t *out, int h, int w) {
 
 	switch (m) {
 	case SPATIAL:
+	case SOBEL:
 		uint32_t *din;
 		cudaMalloc(&din, size);
 		cudaMemcpy(din, in, size, cudaMemcpyHostToDevice);
-		denoiseKernelSpatial<<<blocksPerGrid, threadsPerBlock>>>(
-				din, dout, h, w, perThread);
+		switch (m) {
+		case SPATIAL:
+			denoiseKernelSpatial<<<blocksPerGrid, threadsPerBlock>>>(
+					din, dout, h, w, perThread);
+			break;
+		case SOBEL:
+			double *dsobel = 0;
+			int ssize = size / 4 * sizeof(double);
+			/*
+			if (frame == 2) {
+				cudaMalloc(&dsobel, ssize);
+			}
+			*/
+			denoiseKernelSobel<<<blocksPerGrid, threadsPerBlock>>>(
+					din, frame, dsobel, dout, h, w, perThread);
+			/*
+			if (frame == 2) {
+				double *sobel = (double*) malloc(ssize);
+				cudaMemcpy(sobel, dsobel, ssize, cudaMemcpyDeviceToHost);
+				cudaFree(dsobel);
+				int k;
+				for (k = 0; k < size / 4; k++) {
+					printf("%g\n", sobel[k]);
+				}
+				free(sobel);
+			}
+			*/
+			break;
+		}
 		cudaFree(din);
 		break;
+	case DIFF:
 	case TEMPORAL_AVG:
 	case ADAPTIVE_TEMPORAL_AVG:
 	case KNN:
 	case ZLOKOLICA:
+	case MOTION:
 		// Cyclically right shift backlog
 		for (int i = BACKLOG_SIZE; i > 0; i--) {
 			ctx->backlogs[i] = ctx->backlogs[i-1];
@@ -277,6 +560,18 @@ void denoise(void *p, int m, const uint32_t *in, uint32_t *out, int h, int w) {
 		}
 		cudaMemcpy(ctx->backlogs[0], in, size, cudaMemcpyHostToDevice);
 
+		switch (m) {
+		case ZLOKOLICA:
+		case MOTION:
+			for (int i = NDIFFS; i > 0; i--) {
+				ctx->diffs[i] = ctx->diffs[i-1];
+			}
+			ctx->diffs[0] = ctx->diffs[NDIFFS];
+			if (ctx->diffs[0] == 0) {
+				cudaMalloc(&ctx->diffs[0], size);
+			}
+		}
+
 		KernelContext *dctx;
 		cudaMalloc(&dctx, sizeof(KernelContext));
 		cudaMemcpy(dctx, ctx, sizeof(KernelContext), cudaMemcpyHostToDevice);
@@ -284,6 +579,10 @@ void denoise(void *p, int m, const uint32_t *in, uint32_t *out, int h, int w) {
 		for (i = 0; i < BACKLOG_SIZE && ctx->backlogs[i]; i++)
 			;
 		switch (m) {
+		case DIFF:
+			denoiseKernelDiff<<<blocksPerGrid,
+				threadsPerBlock>>>(dctx, i, dout, h, w, perThread);
+			break;
 		case TEMPORAL_AVG:
 			denoiseKernelTemporalAvg<<<blocksPerGrid,
 				threadsPerBlock>>>(dctx, dout, h, w, perThread);
@@ -293,8 +592,16 @@ void denoise(void *p, int m, const uint32_t *in, uint32_t *out, int h, int w) {
 				threadsPerBlock>>>(dctx, i, dout, h, w, perThread);
 			break;
 		case ZLOKOLICA:
+			updateDiff<<<blocksPerGrid,
+				threadsPerBlock>>>(dctx, h, w, perThread);
 			denoiseKernelZlokolica<<<blocksPerGrid,
-				threadsPerBlock>>>(dctx, dout, h, w, perThread);
+				threadsPerBlock>>>(dctx, i, dout, h, w, perThread);
+			break;
+		case MOTION:
+			updateDiff<<<blocksPerGrid,
+				threadsPerBlock>>>(dctx, h, w, perThread);
+			denoiseKernelMotion<<<blocksPerGrid,
+				threadsPerBlock>>>(dctx, i, dout, h, w, perThread);
 			break;
 		case ADAPTIVE_TEMPORAL_AVG:
 			denoiseKernelAdaptiveTemporalAvg<<<blocksPerGrid,
@@ -307,6 +614,7 @@ void denoise(void *p, int m, const uint32_t *in, uint32_t *out, int h, int w) {
 
 	cudaMemcpy(out, dout, size, cudaMemcpyDeviceToHost);
 	cudaFree(dout);
+	frame++;
 }
 
 void *kernelInit() {
@@ -318,6 +626,9 @@ void kernelFinalize(void *p) {
 	KernelContext *ctx = (KernelContext*) p;
 	for (int i = 0; i < BACKLOG_SIZE; i++) {
 		cudaFree(ctx->backlogs[i]);
+	}
+	for (int i = 0; i < NDIFFS; i++) {
+		cudaFree(ctx->diffs[i]);
 	}
 	free(ctx);
 }
